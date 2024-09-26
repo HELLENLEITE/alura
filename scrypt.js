5cb7b5cb6079ac9d8d159adf730e14a59a663927
HELLENLEITE <127758383+HELLENLEITE@users.noreply.github.com>
2024-09-26 09:51:48 -0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,59 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergumta 1"
+        alternativas: [
+            "Alternativa 1"
+            "Alternativa 2"
+        ]
+    },
+]
+
+const perguntas = [
+    {
+        enunciado: "Pergumta 2"
+        alternativas: [
+            "Alternativa 1"
+            "Alternativa 2"
+        ]
+    },
+]
+
+const perguntas = [
+    {
+        enunciado: "Pergumta 3"
+        alternativas: [
+            "Alternativa 1"
+            "Alternativa 2"
+        ]
+    },
+]
+
+const perguntas = [
+    {
+        enunciado: "Pergumta 4"
+        alternativas: [
+            "Alternativa 1"
+            "Alternativa 2"
+        ]
+    },
+]
+
+const perguntas = [
+    {
+        enunciado: "Pergumta 5"
+        alternativas: [
+            "Alternativa 1"
+            "Alternativa 2"
+        ]
+    },
+]
+
+
+
+
